d740372eb5a497538e051fde841bd974fd526ca1
JosephChataignon <j.chataignon@hotmail.fr>
2019-06-03 13:27:58 +0800

    Ajout barre de titre, navigation laterale et bouton de traduction

--- dictionnaires.js
+++ dictionnaires.js
@@ -1,7 +1,15 @@
 
-
+var fr = {
+    elmt1 : "un premier élément",
+    elmt2 : "un second élément"
+};
 
 var en = {
     elmt1 : "a first element",
     elmt2 : "a second element"
+};
+
+var it = {
+    elmt1 : "un primo elemento",
+    elmt2 : "un secundo elemento"
 };
