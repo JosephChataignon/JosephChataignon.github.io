37da1c7613e9eaecd7be107da80a18bf9aaf1d7f
JosephChataignon <j.chataignon@hotmail.fr>
2019-06-03 16:23:03 +0800

    scrollspy fonctionnel

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
+
+/*$(document).ready(function(){
+    $('#main').scrollspy({target: ".sidenav", offset: 50});
+});*/
+
+
+
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
 
 function traduire(langue) {
