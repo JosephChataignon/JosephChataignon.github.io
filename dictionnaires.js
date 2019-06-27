8ac252632c3355f495acfa1de406a6d9bf2ca0c6
JosephChataignon <j.chataignon@hotmail.fr>
2019-06-27 19:39:50 +0800

    remplissage paragraphe prepas

--- dictionnaires.js
+++ dictionnaires.js
@@ -59,7 +59,25 @@ var en = {
     codeSource : "I created this website by myself, the source code is",
     libreAccesGithub : "freely accessible on Github",
     texteInfobulleAge : "born June 24, 1997",
-    age : "age 21"
+    age : "age 21",
+    lienProfilLinkedin : "Go to my Linkedin profile",
+    lienProfilGithub : "Go to my Github profile",
+    titrePageTop : "Page top",
+    titreAPropos : "About me",
+    titreParcours : "Academic background",
+    titreCompetences : "Skills",
+    navTitreNCTU : "Chiao Tung University",
+    navTitreTSE : "Télécom Saint-Etienne",
+    navTitreISM : "Sainte-Marie d'Antony",
+    paragAPropos : "lorem ipsum etc...",
+    titreNCTU : "National Chiao Tung University, Hsinchu, Taïwan",
+    paragNCTU : "Chiao Tung University...",
+    titreTSE : "Télécom Saint-Etienne, Saint-Etienne, France",
+    paragTSE : "Télécom Saint-Etienne is a school...",
+    titreISM : "Institution Sainte-Marie, Antony, France",
+    paragISM : "The ISM...",
+    paragPrepas : "Les classes préparatoires aux grandes écoles (CPGE) sont des filières d'enseignement supérieur. Hébergées généralement dans des lycées, elles ont pour but de préparer les étudiants aux concours des grandes écoles. <br> Considérées comme hautement exigentes et sélectives, la compétitivité des concours auxquels les CPGE emmènent leur étudiants les pousse à exiger de ceux-ci une charge de travail parmi les plus hautes d'Europe.",
+    telechCV : "<i class=\"far fa-file-pdf\"></i> See resume"
 };
 
 var it = {
