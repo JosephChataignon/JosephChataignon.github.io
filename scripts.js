3f8f17bfdd9a504e69a9138bb539ee5f7bcb4086
JosephChataignon <j.chataignon@hotmail.fr>
2019-07-01 21:27:57 +0800

    fonction pour obtenir l'age

--- scripts.js
+++ scripts.js
@@ -37,3 +37,15 @@ function hautDePage() {
   document.body.scrollTop = 0;              // For Safari
   document.documentElement.scrollTop = 0;   // For Chrome, Firefox, IE and Opera
 }
+
+// Donne l'âge (issu de stackoverflow.com/questions/10008050)
+function getAge() {
+    var today = new Date();
+    var birthDate = new Date('1997/06/24');
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
